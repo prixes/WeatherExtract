6fd95bf26dd3d2f680467fe04b9cf9258184c908
David Parushev <davidn.1107@gmail.com>
2017-03-23 15:32:30 +0200

    + Action Bar added + Google maps api key + Get weather button on the map view + Marker info onClick + Marker always on center of the screen + smooth when scrolling and zooming the map

--- app/src/main/java/com/prixesoft/david/weatherextract/MapsActivity.java
+++ app/src/main/java/com/prixesoft/david/weatherextract/MapsActivity.java
@@ -1,23 +1,43 @@
 package com.prixesoft.david.weatherextract;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Pair;
+import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
     private GoogleMap mMap;
-
+    private Marker marker;
+    private double latitude,longitude;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.action_bar_layout);
+
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -36,11 +56,60 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
+
+
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+
+        // Add a marker in Sofia and move the camera
+        LatLng sofia = new LatLng(42.697705, 23.321638);
+        marker = mMap.addMarker(new MarkerOptions().position(sofia).title("Marker in Sofia"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sofia));
+
+        mMap.setOnCameraMoveListener (new GoogleMap.OnCameraMoveListener () {
+            @Override
+            public void onCameraMove() {
+                mMap.clear();
+                marker = mMap.addMarker(new MarkerOptions().position(mMap.getCameraPosition().target));
+            }
+
+
+        });
+
+        googleMap.setOnMarkerClickListener(this);
+
+
     }
+
+
+    void addLocation(){
+        longitude = mMap.getCameraPosition().target.longitude;
+        latitude =  mMap.getCameraPosition().target.latitude;
+    }
+
+
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        if (marker.equals(marker))
+        {
+            addLocation();
+            marker.setTitle("Current Selection");
+            marker.setSnippet("Lat:" + latitude + " Long:" + longitude);
+        }
+
+        return false;
+    }
+
+
+    public void onButtonClick(View view){
+        addLocation();
+        Intent intentWeatherInfo = new Intent(MapsActivity.this,WeatherInfo.class);
+        intentWeatherInfo.putExtra("longitude",longitude);
+        intentWeatherInfo.putExtra("latitude",latitude);
+        startActivity(intentWeatherInfo);
+
+    }
+
 }
